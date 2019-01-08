cdadfa1633991108739730941b8a1b74b0cffeab
Ashvarya Singh <ashvarya.singh@appster.in>
2019-01-08 22:46:33 +0530

    Initial Commit

---
+++ CodingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingTest
+//
+//  Created by Ashvarya Singh on 08/01/19.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
